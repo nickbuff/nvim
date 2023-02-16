f9b19ec4a7a881f32d8ee16a68aaeeaf10c0ae91
Nick Bugaiov <nick@bugaiov.com>
2023-02-16 16:05:14 +0100

    update

--- init.lua
+++ init.lua
@@ -26,15 +26,19 @@ vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 
 vim.opt.updatetime = 50
+vim.opt.timeoutlen = 100
 
 vim.opt.colorcolumn = "80"
+vim.opt.clipboard = "unnamedplus"
 
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>w", "<cmd>w<CR>")
-vim.keymap.set("n", "<leader>q", "<cmd>q<CR>")
-vim.keymap.set("i", "kj", "<Esc>")
-vim.keymap.set("i", "jj", "<Esc>")
-vim.keymap.set("i", "kj", "<Esc>")
+
+vim.keymap.set("n", "<leader>w", "<cmd>w<cr>")
+vim.keymap.set("n", "<leader>q", "<cmd>q<cr>")
+
+-- vim.keymap.set("i", "kj", "<esc>")
+-- vim.keymap.set("i", "jj", "<esc>")
+-- vim.keymap.set("i", "kj", "<esc>")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
@@ -44,6 +48,15 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("i", "<C-c>", "<Esc>")
+vim.keymap.set({"i", "n"}, "<leader>c", "<cmd>e ~/.config/nvim/init.lua<cr>")
+
+vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+vim.keymap.set('n', '<leader>a', ':keepjumps normal! ggVG<cr>')
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -59,12 +72,14 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
+    -- Themes
 	{
 		'navarasu/onedark.nvim',
 		config = function()
 			require('onedark').load()
         end
     },
+    -- Editing
     {
         "echasnovski/mini.comment",
         event = "VeryLazy",
@@ -72,10 +87,40 @@ require("lazy").setup({
             require("mini.comment").setup()
         end,
     },
+		--   {
+		--       'vladdoster/remember.nvim',
+		-- config = function()
+		-- 	require('remember')
+		-- end
+		--   },
+    -- TreeSitter
     {
-        'vladdoster/remember.nvim',
-		config = function()
-			require('remember')
-		end
-	},
+        "nvim-treesitter/nvim-treesitter",
+        version = false,
+        build = ":TSUpdate",
+        event = "BufReadPost",
+        opts = {
+            highlight = { enable = true },
+            indent = { enable = true },
+            context_commentstring = { enable = true, enable_autocmd = false },
+        },
+        config = function(_, opts)
+            require("nvim-treesitter.configs").setup(opts)
+        end,
+    },
+    -- LSP Support 
+    -- TODO: Finish the setup
+    {
+        'neovim/nvim-lspconfig',
+    },
+    {
+        'williamboman/mason.nvim',
+        config = function()
+            require("mason").setup()
+        end
+    },
+    {
+        'williamboman/mason-lspconfig.nvim'
+    },
+
 })
